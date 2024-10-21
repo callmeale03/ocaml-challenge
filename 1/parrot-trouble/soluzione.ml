9d8bbee43887bdc0f2c4591eb12cd2d506c92bcf
callmeale03 <lssndrlai@gmail.com>
2024-10-21 12:58:28 +0200

    soluzione.ml

---
+++ 1/parrot-trouble/soluzione.ml
@@ -0,0 +1,7 @@
+let parrot_trouble (talk : bool) (time : int) = 
+  if time < 0 || time > 23 then None else (* Viene controllato se il tempo inserito è un valore reale *)
+  (* Il pattern matching verifica se il pappagallo parla o meno durante un orario proibito *)
+  match talk with
+  | true -> if time < 7 || time > 20 then Some true else Some false
+  | false -> Some false
+  ;;
