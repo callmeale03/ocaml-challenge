20d863640db74418dcf7704369da0aadcba68785
callmeale03 <lssndrlai@gmail.com>
2024-11-26 18:56:25 +0100

    soluzione.ml

---
+++ 1/7-11-doubles/soluzione.ml
@@ -0,0 +1,8 @@
+let seven_eleven_doubles () = 
+  let d1 = Random.int(6) + 1 in
+  let d2 = Random.int(6) + 1 in
+  if d1 = d2 || d1 + d2 = 7 || d1 + d2 = 11 then 
+    (true, d1, d2)
+  else
+    (false, d1, d2)
+;;
